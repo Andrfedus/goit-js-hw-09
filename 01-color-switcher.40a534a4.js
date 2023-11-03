5d65cfd4465de42b1e6e544d4bbb462176aead35
Andrfedus <116153021+Andrfedus@users.noreply.github.com>
2023-11-03 14:04:39 +0000

    Deploying to gh-pages from @ Andrfedus/goit-js-hw-09@cef1a7d06c59b8f329167e0e3cad3d8fdcae5f8e 🚀

---
+++ 01-color-switcher.40a534a4.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.body,a=null;t.addEventListener("click",(function(e){t.disabled=!0,a=setInterval((function(){n.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(a)}))}();
+//# sourceMappingURL=01-color-switcher.40a534a4.js.map
---
+++ 01-color-switcher.be41ed40.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=document.body;let n=null;t.addEventListener("click",(function(e){t.disabled=!0,n=setInterval((()=>{a.style.background=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(n)}));
+//# sourceMappingURL=01-color-switcher.be41ed40.js.map

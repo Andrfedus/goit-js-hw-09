998a96f35e0e5a5cc6248e7e10fc6502e1f503d0
Andrfedus <116153021+Andrfedus@users.noreply.github.com>
2023-11-13 10:25:32 +0200

    fixed-error-json

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,37 +1,31 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 
-
-const btnStart = document.querySelector('btn-timer');
-
-const days = document.querySelector('[data-days]');
-const hours = document.querySelector('[data-hours]');
-const minutes = document.querySelector('[data-minutes]');
-const seconds = document.querySelector('[data-seconds]');
-
-btnStart.disabled = true;
-let timeId = null;
+const btnTimerStart = document.querySelector('[data-start]');
+const timerFieldDays = document.querySelector('[data-days]');
+const timerFielHours = document.querySelector('[data-hours]');
+const timerFieldMinutes = document.querySelector('[data-minutes]');
+const timerFieldSeconds = document.querySelector('[data-seconds]');
 
 
-// Notiflix.Notify.info('Please friend, choise data');
-
-
+btnTimerStart.disabled = true;
+let timerId = null;
 
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
+
   onClose(selectedDates) {
-    const date = new Date();
-    
-    if (selectedDates[0] - date > 0) {
-      btnStart.disabled = false;
-    }
-    else {
-      btnStart.disabled = true;
+    const currentDate = new Date();
+
+    if (selectedDates[0] - currentDate > 0) {
+      refs.btnTimerStart.disabled = false;
+    } else {
+      refs.btnTimerStart.disabled = true;
       Notify.failure('Please choose a date in the future', {
         timeout: 1500,
         width: '400px',
@@ -41,19 +35,14 @@ const options = {
 };
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
@@ -63,37 +52,29 @@ function addLeadingZero(value) {
   return String(value).padStart(2, 0);
 }
 
-function onTimeStart() {
+function onTimerStart() {
   const selectedDate = fp.selectedDates[0];
 
-  timeId = setInterval(() => {
+  timerId = setInterval(() => {
     const startTime = new Date();
-    const timer = selectedDate - startTime;
-   
-    btnStart.disabled = true;
+    const countdown = selectedDate - startTime;
+    btnTimerStart.disabled = true;
 
-    if (timer < 0) {
-      clearInterval(timeId);
+    if (countdown < 0) {
+      clearInterval(timerId);
       return;
     }
-    updateTimerDates(convertMs(timer));
-  },1_000)
+    updateTimerFace(convertMs(countdown));
+  }, 1_000);
 }
 
-function updateTimerDates() {
-  days.textContent = addLeadingZero(days);
-  hours.textContent = addLeadingZero(hours);
-  minutes.textContent = addLeadingZero(minutes);
-  seconds.textContent = addLeadingZero(seconds);
+function updateTimerFace({ days, hours, minutes, seconds }) {
+ timerFieldDays.textContent = addLeadingZero(days);
+  timerFielHours.textContent = addLeadingZero(hours);
+  timerFieldMinutes.textContent = addLeadingZero(minutes);
+  timerFieldSeconds.textContent = addLeadingZero(seconds);
 }
-
 
 const fp = flatpickr('#datetime-picker', options);
 
-
-btnStart.addEventListener('click', onTimeStart);
-
-
-
-
-
+btnTimerStart.addEventListener('click', onTimerStart);
